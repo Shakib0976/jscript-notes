6bd4553996de177ed5090163e18b68cae3574a25
Shakib0976 <web.shakib09@gmail.com>
2025-02-05 18:31:12 +0600

    this is nambure +

--- variavle.js
+++ variavle.js
@@ -1,8 +1,22 @@
-var car= "shakib";
-
-console.log(car);
-
 // String type variable
 
+var car= "shakib";
+
+console.log( typeof car);
+
+// bolean
+
 var isrich= true ;
-console.log(isrich);
+console.log( typeof isrich);
+
+
+var nambur=897;
+console.log(typeof nambur);
+
+
+
+var orange=555;
+var banana=9898;
+
+console .log(orange+banana);
+
